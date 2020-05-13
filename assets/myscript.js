ad1956b2334815c8006c4dd3d0c50470cbb0a354
Shororo <akagimaki@yahoo.co.jp>
2020-05-14 01:55:14 +0900

    new

--- assets/myscript.js
+++ assets/myscript.js
@@ -1,8 +1,7 @@
 // ヘッダーの設定
+
+//ナビゲーションバーを推した時、画面外を推した時
 $(function(){
-  
-//  if (winW <= devW) {
-    //720px以下の時の処理
     $(document).on('click',function(e) {
      if(!$(e.target).closest('.navigation').length) {
      // ターゲット要素の外側をクリックした時の操作
@@ -22,6 +21,46 @@ $(function(){
  });
 	//スマホ用navここまで
 
+});
+
+
+//ナビゲーションメニューのいずれかを推した時
+$(document).ready(function(){
+  //URLのハッシュ値を取得
+  var urlHash = location.hash;
+  //ハッシュ値があればページ内スクロール
+  if(urlHash) {
+    //スクロールを0に戻す
+    $('body,html').stop().scrollTop(0);
+    setTimeout(function () {
+      //ロード時の処理を待ち、時間差でスクロール実行
+      scrollToAnker(urlHash) ;
+    }, 100);
+  }
+  //通常のクリック時
+  $('a[href^="#"]').click(function() {
+    //ページ内リンク先を取得
+    var href= $(this).attr("href");
+    //リンク先が#か空だったらhtmlに
+    var hash = href == "#" || href == "" ? 'html' : href;
+    //スクロール実行
+    scrollToAnker(hash);
+    //リンク無効化
+    return false;
+  });
+
+  // 関数：スムーススクロール
+  // 指定したアンカー(#ID)へアニメーションでスクロール
+  function scrollToAnker(hash) {
+    if($('.nav-mobile').css("display") =="block"){
+     $(".nav-mobile").slideUp();
+     $(".nav-cover").slideUp();
+     $('.navigation').toggleClass("pushed");
+   }
+    var target = $(hash);
+    var position = target.offset().top;
+    $('body,html').stop().animate({scrollTop:position}, 500);
+  }
 });
 
 
